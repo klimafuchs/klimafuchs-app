4e4edeba0b5a956df401d7158412552f024b968c
Timo Volk <timo.volk@gmail.com>
2019-04-24 13:33:53 +0200

    localization provider now emitts warnings if template values are not specified

--- src/localization/LocalizationProvider.js
+++ src/localization/LocalizationProvider.js
@@ -2,15 +2,25 @@
 import {dev, prod} from '../env';
 import * as localization from '../localization/locale.gen'
 let env = process.env.NODE_ENV !== "production" ? dev : prod;
-let lang = env.lang || "default";
+let lang = env.LANG || "default";
 
+/**
+ * Resolves string names to translated string constants defined in locale.gen.json
+ * The Locale file is generated with the script APP_ROOT/get-copy.sh which loads the
+ * definitions from a google sheets document at the COPY_SHEET_URL url
+ */
 export class LocalizationProvider {
 
+    /**
+     * Resolves a given string name to a constant or a filled template
+     * @param key name
+     * @param values template values in ${} blocks
+     * @returns {string|*} the string constant or the filled template
+     */
     static get(key, values) {
         let template = localization[key];
         if(template) {
             if(template[lang]) {
-                if(!values) return template[lang];
                 return this.makeTemplateString(template[lang], values)
             }
             else {
@@ -26,8 +36,14 @@ export class LocalizationProvider {
     }
 
     static makeTemplateString(string, values) {
-        console.log(values)
-        return new Function(...Object.keys(values), "return `"+string+"`;")(...Object.values(values));
+        try {
+            values = values || {};
+            return new Function(...Object.keys(values), "return `"+string+"`;")(...Object.values(values));
+        } catch (e) {
+            console.warn(e.message);
+            return string;
+
+        }
     }
 }
 
