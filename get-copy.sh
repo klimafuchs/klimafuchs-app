c7114c42e857b715a10ace9bf1303736576e3a02
Timo Volk <timo.volk@gmail.com>
2019-04-24 11:54:52 +0200

    loads a localization file from google sheets

---
+++ get-copy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+if [[ ! -f .env ]]
+then
+  source .env
+fi
+
+URL="https://docs.google.com/spreadsheets/u/1/d/1Kge9H9MNjEFVsvL-HXzd9kbV9PBqQ-r223VQnDjtl3g/export?format=csv&id=1Kge9H9MNjEFVsvL-HXzd9kbV9PBqQ-r223VQnDjtl3g&gid=0"
+DEST_FILE=./src/localization/locale.gen.json
+
+LOCALE=$(curl --silent ${URL} 2>&1 | csvtojson | ./genLocale.js)
+echo "$LOCALE" > ${DEST_FILE}
